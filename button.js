066279efa0712089fc23ebaf524e921fb927682b
TakakiRei <132850757+TakakiRei@users.noreply.github.com>
2023-11-27 02:29:33 +0900

    Update button.js

--- button.js
+++ button.js
@@ -7,19 +7,19 @@ let switchButton;
 
 let buttonObj = {
   buttons: [[], [], [], [], []],
-  previousScreen: 0,
-  setButton: function (screen, label, w, posX, posY, func) {
+  pScreen: 0,
+  setButton: function (screen, label, posX, posY, w, func) {
     let button = createButton(label);
-    button.style("width", w + "px");
     button.addClass("myButtonStyle");
     button.position(posX, posY);
+    button.style("width", w + "px");
     button.mouseClicked(func);
     this.buttons[screen].push(button);
     return button;
   },
   buttonShow: function (screen) {
-    if (this.previousScreen != screen) {
-      let obj = this.buttons[this.previousScreen];
+    if (this.pScreen != screen) {
+      let obj = this.buttons[this.pScreen];
       for (let btn of obj) {
         btn.hide();
       }
@@ -27,7 +27,7 @@ let buttonObj = {
       for (let btn of obj) {
         btn.show();
       }
-      this.previousScreen = screen;
+      this.pScreen = screen;
     }
   },
 };
@@ -61,24 +61,29 @@ function partSetting(part) {
 }
 
 function setButtons(){
+  const leftBtnPosX = 10;
+  const leftBtnwidth = 80;
+  const rightBtnPosX = 530;
+  const rightBtnwidth = 100;
+  const bottomBtnPosY = 430;
   let backButton;
   
-  buttonObj.setButton(1, "<ruby>設定<rt>せってい</rt></ruby>", 80, 10, 430, () => {screen = 2;});
-  backButton = buttonObj.setButton(2, "<ruby>戻<rt>もど</rt></ruby>る", 80, 10, 430, () => {screen--;});
-  buttonObj.setButton(2, "リセット", 100, 530, 430, init);
-  buttonObj.setButton(2, joint.label, 110, 120, 300, () => {screen = 4;});
-  switchButton = buttonObj.setButton(2, offLabel, 110, 350, 300, trajectorySwitching);
+  buttonObj.setButton(1, "<ruby>設定<rt>せってい</rt></ruby>", leftBtnPosX, bottomBtnPosY, leftBtnwidth, () => {screen = 2;});
+  backButton = buttonObj.setButton(2, "<ruby>戻<rt>もど</rt></ruby>る", leftBtnPosX, bottomBtnPosY, leftBtnwidth, () => {screen--;});
+  buttonObj.setButton(2, "リセット", rightBtnPosX, bottomBtnPosY, rightBtnwidth, init);
   buttonObj.buttons[3].push(backButton);
-  resetButton = buttonObj.setButton(3, "リセット", 100, 530, 430, () => {});
-  buttonObj.setButton(4, "<ruby>戻<rt>もど</rt></ruby>る", 80, 10, 430, () => {screen = 2;});
-  buttonObj.setButton(4, "リセット", 100, 530, 430, jointInit);
+  resetButton = buttonObj.setButton(3, "リセット", rightBtnPosX, bottomBtnPosY, rightBtnwidth, () => {});
+  buttonObj.setButton(4, "<ruby>戻<rt>もど</rt></ruby>る", leftBtnPosX, bottomBtnPosY, leftBtnwidth, () => {screen = 2;});
+  buttonObj.setButton(4, "リセット", rightBtnPosX, bottomBtnPosY, rightBtnwidth, jointInit);
   
-  let Y = btnPosY;
+  let posY = btnPosY;
   for (let i in parts) {
     let part = parts[i];
     let bool = i % 2;
-    let X = btnPosX + btnDeltaX * bool;
-    buttonObj.setButton(2, handObj[part].label, btnWidth, X, Y, partSetting(part));
-    Y += btnDeltaY * bool;
+    let posX = btnPosX + btnDeltaX * bool;
+    buttonObj.setButton(2, handObj[part].label, posX, posY, btnWidth, partSetting(part));
+    posY += btnDeltaY * bool;
   }
+  buttonObj.setButton(2, joint.label, btnPosX, posY, btnWidth, () => {screen = 4;});
+  switchButton = buttonObj.setButton(2, offLabel, btnPosX + btnDeltaX, posY, btnWidth, trajectorySwitching);
 }
